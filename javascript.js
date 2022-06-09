1639a4dadc31c0c89a61366f403a3eec03850f35
Nuchi <nadinajauch@gmail.com>
2022-06-09 11:43:00 -0300

    Agrego fecha y pagos en efectivo o no

--- javascript.js
+++ javascript.js
@@ -1,25 +1,44 @@
 let total = 0;
 let promedio = 0;
 let lista = [];
+let efectivoCheck = false;
+let efectivo = "";
 
+function pagoEfectivo() {
+  efectivoCheck = !efectivoCheck;
+}
 function agregarMovimiento() {
   if ($("#nombre").val() != "" && $("#gasto").val() != "") {
     let objeto = new Object();
-
     objeto.nombre = $("#nombre").val();
     objeto.gasto = $("#gasto").val();
+    objeto.fecha = new Date().toLocaleString();
     lista.push(objeto);
 
+    if (efectivoCheck == true) {
+      console.log("Hola");
+      efectivo = "Pagó en efectivo";
+    } else {
+      efectivo = "No pagó en efectivo";
+    }
     $("#grupo-list").append(
-      '<li class="bg-light border rounded  w-50" >' +
+      '<li class="bg-light border rounded  d-flex" ><ul>' +
+        "<li>" +
         objeto.nombre +
         ": $" +
         objeto.gasto +
-        "</li>"
+        ", " +
+        "</li>" +
+        "<li>" +
+        efectivo +
+        "</li>" +
+        "<li>Fecha: " +
+        objeto.fecha +
+        "</li>" +
+        "</ul></li>"
     );
 
     calcularTotales();
-
     $("#total").html("<br>Total: $" + total);
     $("#promedio").html("Cada uno debe aportar: $" + promedio);
     $("#nombre, #gasto").val("");
